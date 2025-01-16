696cd17413a46dbaede45fe1d43ff9b359f3f943
sohanur <sohanurshantorahman@gmail.com>
2025-01-16 23:49:28 +0600

    added is_authenticated in the custom user class

--- shared_auth/permissions.py
+++ shared_auth/permissions.py
@@ -2,12 +2,20 @@ from rest_framework.permissions import BasePermission
 
 class IsVendor(BasePermission):
     def has_permission(self, request, view):
+        if not request.user or not request.user.is_authenticated:
+            return False
         return request.user.is_vendor()
+
 
 class IsVenueOwner(BasePermission):
     def has_permission(self, request, view):
+        if not request.user or not request.user.is_authenticated:
+            return False
         return request.user.is_venue_owner()
+
 
 class IsVenueManager(BasePermission):
     def has_permission(self, request, view):
+        if not request.user or not request.user.is_authenticated:
+            return False
         return request.user.is_venue_manager()
